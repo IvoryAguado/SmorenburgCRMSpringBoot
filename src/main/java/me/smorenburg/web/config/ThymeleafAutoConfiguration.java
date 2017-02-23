92036972a92229c590b286fc1e1e173309874825
MRX\MR x <ivorsmorenburgaguado@gmail.com>
2017-02-23 15:55:12 +0000

    Merge remote-tracking branch 'origin/master'

--- src/main/java/me/smorenburg/web/config/ThymeleafAutoConfiguration.java
+++ src/main/java/me/smorenburg/web/config/ThymeleafAutoConfiguration.java
@@ -38,7 +38,7 @@ import java.util.Collections;
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class ThymeleafAutoConfiguration {
 
-    public static final String DEFAULT_PREFIX = "classpath:/templates/";
+    public static final String DEFAULT_PREFIX = "classpath:templates/";
 
     public static final String DEFAULT_SUFFIX = ".html";
 
